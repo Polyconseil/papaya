72262ab8e2b34ba52eefce70c4fbd63023f40bf2
Raphaël Barrois <raphael.barrois@polytechnique.org>
2017-06-17 20:53:48 +0200

    Fix Python2 support.

--- dev/settings.py
+++ dev/settings.py
@@ -10,6 +10,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
+from __future__ import unicode_literals
+
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
